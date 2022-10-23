c9d6a4709120f1e33cf938f047e2b758f81cef97
NdekoCode <arickbulakali@gmail.com>
2022-10-23 22:22:01 +0200

    learn: Read a directory and show contents property

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-import { stat, open } from "node:fs/promises"; // On n'importe le module FS avec les Promise
-try {
-  const info = await stat("./demo.txt");
-  if (info) {
-    const file = await open("./demo.txt", "a");
-    file.write("Apprendre le developpement");
-    file.close();
-  }
-  console.log(info);
-} catch (err) {
-  console.log("Erreur", err);
+import { readdir, stat } from "node:fs/promises";
+console.time("code");
+function getSizeToKb(v) {
+  return parseFloat(v.size).toFixed(1) + "kb";
 }
+function getInfos(file, fileInfo) {
+  return file.isDirectory()
+    ? `D - ${file.name}`
+    : `F - ${file.name} - ${getSizeToKb(fileInfo)}`;
+}
+const files = await readdir("./", { withFileTypes: true });
+await Promise.allSettled(
+  files.map(async (file) => {
+    const infos = await stat(file.name);
+    console.log(getInfos(file, infos));
+  })
+);
+console.timeEnd("code");
