f08dcdf26b3943fc4350775799380e0a024f2138
NdekoCode <arickbulakali@gmail.com>
2022-10-23 21:03:29 +0200

    learn: git stat for get information and open, write and close function for read and write file properly

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-import { readFile, writeFile } from "node:fs/promises"; // On n'importe le module FS avec les Promise
-console.log("Node.js");
-await writeFile("./demo.txt", "Bonjour les gens \n", {
-  flag: "a+",
-});
-//
-// console.log(content);
+import { stat, open } from "node:fs/promises"; // On n'importe le module FS avec les Promise
+try {
+  const info = await stat("./demo.txt");
+  if (info) {
+    const file = await open("./demo.txt", "a");
+    file.write("Apprendre le developpement");
+    file.close();
+  }
+  console.log(info);
+} catch (err) {
+  console.log("Erreur", err);
+}
