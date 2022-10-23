83e1aac8bb2ed48b683a9e7d88967de709404217
NdekoCode <arickbulakali@gmail.com>
2022-10-23 11:09:22 +0200

    learn: read file using Promise fs module and Promise.all

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-import fs, { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises"; // On n'importe le module FS avec les Promise
 console.log("Node.js");
-const content = readFileSync("./demo.txt", { encoding: "utf-8" });
+const content = await Promise.all([
+  readFile("./demo.txt", { encoding: "utf-8" }),
+  readFile("./app.js", { encoding: "utf-8" }),
+]);
 console.log(content);
-fs.readFile("./demo.txt", { encoding: "utf-8" }, (err, data) => {
-  if (err) return err;
-  console.log(data);
-});
