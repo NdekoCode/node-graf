65a4870e4803ba19fae3823efac0a7d299952c45
NdekoCode <arickbulakali@gmail.com>
2022-10-23 23:36:02 +0200

    learn: stream write and read stream and pipe function

--- stream.js
+++ stream.js
@@ -1,3 +1,15 @@
-import { readFile, writeFile } from "node:fs/promises";
-const content = await readFile("./video.mp4");
-await writeFile("video-copy.mp4", content);
+import { createReadStream, createWriteStream } from "node:fs";
+import { stat } from "node:fs/promises";
+let read = 0;
+const file = "./video.mp4";
+const { size } = await stat(file);
+const stream = createReadStream(file);
+stream.on("data", (chunk) => {
+  read += chunk.length;
+  console.log(Math.round((read / size) * 100));
+});
+stream.on("close", () => {
+  console.log("Closed");
+});
+const write = createWriteStream("video-copy.mp4");
+stream.pipe(write); // On créer une copie et c'est bien plus performant car on a pas été obliger de sauvegarder l'entiereté du fichier en memoire
