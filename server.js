186fa35696676db7e224a8fe61b67933fe99f379
NdekoCode <arickbulakali@gmail.com>
2022-10-24 18:39:06 +0200

    parse URL to get url params

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-import { createReadStream } from "node:fs";
 import { createServer } from "node:http";
 const port = process.env.PORT || 3000;
 const server = createServer((req, res) => {
-  console.log(req.url, req.statusCode, req.statusMessage, req.method);
-  const file = createReadStream("index.html");
-  // Le serveur renvois une en-tete contenant le status de la reponse et il dit qu'il envois du text HTML
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-  file.pipe(res, { end: false });
-  file.on("end", () => {
-    res.end();
-  });
+  console.log(req.headers.host);
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  console.log(url);
+  console.log(url.searchParams.get("name"));
+  res.write(`Salut ${url.searchParams.get("name")}`);
+  res.end();
 });
-server.listen(port, () => console.log("listening at the port " + port));
+server.listen(port);
