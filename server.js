2e1721311c87f652d899f44a73d1c2219e46319b
NdekoCode <arickbulakali@gmail.com>
2022-10-24 19:21:13 +0200

    refactor:learn: get data on post request in node.js by parsing to json format

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 import { createServer } from "node:http";
+import { json } from "node:stream/consumers";
 
 const port = process.env.PORT || 3000;
-const server = createServer((req, res) => {
-  let body = "";
-  // On on va detecter
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-  req.on("close", () => {
-    res.write("Salut " + JSON.stringify(body));
-    res.end();
-  });
+const server = createServer(async (req, res) => {
+  const data = await json(req);
+  res.write(`Salut ${data.name}`);
+  res.end();
 });
 server.listen(port);
